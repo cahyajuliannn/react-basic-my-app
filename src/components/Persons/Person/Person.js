e5152a0f043ffd9a028f498f8f5ab64cf89f40c5
cahyajuliannn <cahyajuliannn@gmail.com>
2021-02-14 05:09:04 +0700

    add finale

---
+++ src/components/Persons/Person/Person.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import Auxiliary from "../../../hoc/Auxiliary";
+import withClass from "../../../hoc/withClass";
+import classes from "./Person.module.css";
+import AuthContext from "../../../context/auth-context";
+
+class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    this.inputElementRef.current.focus();
+    console.log(this.context.autheticated);
+  }
+
+  render() {
+    console.log("[Person.js] rendering..");
+    return (
+      <Auxiliary>
+        {this.context.autheticated ? <p>Authenticated</p> : <p>Please Login</p>}
+        <p key="i1" onClick={this.props.click}>
+          I'm {this.props.name} and I am {this.props.age} years old!
+        </p>
+        <p key="i2">{this.props.children}</p>
+        <input
+          key="i3"
+          // ref={(inputEl) => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </Auxiliary>
+    );
+  }
+}
+
+Person.propTypes = {
+  click: PropTypes.func,
+  name: PropTypes.string,
+  age: PropTypes.number,
+  change: PropTypes.func,
+};
+
+export default withClass(Person, classes.Person);
